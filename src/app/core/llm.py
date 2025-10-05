b1e62e0baa359db85c7c51800f3c9fb828f56f42
artemmmz <arzar0046@gmail.com>
2025-10-05 08:36:42 +0300

    FIX CRITICAL BUG

--- src/app/core/llm.py
+++ src/app/core/llm.py
@@ -70,7 +70,36 @@ def create_system_prompt() -> str:
         
         f"Текущее время: {datetime.datetime.now().isoformat()}\n\n"
         
-        f"""Примеры запросов: \n{examples_text}\n"""
+        """Примеры запросов: 
+        Q: Доступна ли покупка акций 'Мечел' на счете 77777?
+        A: API_REQUEST: GET /v1/assets/{symbol:Мечел}/params?account_id=77777
+        
+        Q: Цена последней сделки по ROSN@MISX.
+        A: API_REQUEST: GET: /v1/instruments/ROSN@MISX/quotes/latest
+        
+        Q: Купи 2 фьючерса на газ NGZ5@RTSX по рынку
+        A: API_REQUEST: POST /v1/accounts/{account_id}/orders
+        
+        Q: Покажи расписание клиринга для акций 'МосБиржи'.
+        A: API_REQUEST: GET /v1/assets/{symbol:МосБиржа}/schedule
+        
+        Q: Покажи детали по ордеру ORD314159
+        A: API_REQUEST: GET /v1/accounts/{account_id}/orders/ORD314159
+        
+        Q: Какое гарантийное обеспечение для фьючерса SiZ5@RTSX на счете ACC-001-A?
+        A: API_REQUEST: GET /v1/accounts/ACC-001-A
+        
+        Q: Сколько сделок было совершено в августе 2025?
+        A: API_REQUEST: GET /v1/accounts/{account_id}/trades?interval.start_time=2025-08-01T00:00:00Z&interval.end_time=2025-08-31T23:59:59Z
+        
+        Q: Какой тикер у инструмента с ISIN RU0009029540?
+        A: API_REQUEST: GET /v1/assets
+        
+        Q: Дай мне историю цен на Apple за январь 2025 года с часовым интервалом.;
+        A: API_REQUEST: GET /v1/instruments/{symbol}/bars?timeframe=TIME_FRAME_H1&interval.start_time=2025-01-01T00:00:00Z&interval.end_time=2025-01-31T23:59:59Z
+        
+        Q: Выгрузить все транзакции за последнюю неделю
+        A: API_REQUEST: GET /v1/accounts/FIN-203-B/transactions?interval.start_time=2025-09-22T00:00:00Z&interval.end_time=2025-09-28T23:59:59Z\n"""
 
         """Отвечай на русском, КРАТКО И ПО ДЕЛУ!!\n"""
     )
